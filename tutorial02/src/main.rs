88e382423c372500d89d4b8b2eb5b4a930c03737
ninjaoflight <jonguz5@hotmail.com>
2021-01-30 16:48:26 -0600

    tutorial 2

--- tutorial02/src/main.rs
+++ tutorial02/src/main.rs
@@ -1,14 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("intenta adivinar el numero");
-    println!("Ingrese el numero");
 
-    let mut entrada = String::new();
+    let numero = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin()
-        .read_line(&mut entrada)
-        .expect("no se pudo leer la linea");
+    loop {
+        println!("Ingrese el numero");
 
-    println!("el numero es {}", entrada);
+        let mut entrada = String::new();
+
+        io::stdin()
+            .read_line(&mut entrada)
+            .expect("no se pudo leer la linea");
+
+        let entrada: u32 = match entrada.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("ingrese un numero valido");
+                continue;
+            }
+        };
+        
+        match entrada.cmp(&numero) {
+            Ordering::Less => println!("muy bajo!"),
+            Ordering::Greater => println!("muy alto!"),
+            Ordering::Equal => {
+                println!("Ganaste");
+                break;
+            }
+        }
+
+    }
 }
